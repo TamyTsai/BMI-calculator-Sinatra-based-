828838d4a26b39d3d2b8de9107e04a9368cfd86d
Tamy <tamy8677@gmail.com>
2024-05-25 18:18:33 +0800

    add data validation

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,11 +1,11 @@
-// 判斷是否有欄位未輸入，並跳出對應警示視窗
+// 判斷是否有欄位未輸入，並跳出對應警示視窗，皆有輸入就送到後端
 function Empty() {
     var height = document.querySelector('#bodyHeight').value; // 指定 HTML中id為bodyHeight輸入框 的 輸入值 給 變數height
     var weight = document.querySelector('#bodyWeight').value;
     
     // 如果身高、體重輸入的值 有空的，就跳對應提醒視窗
     if ((height != '') && (weight != '')) {
-        // 不跳提醒
+        document.bodyform.submit();	// 都有填寫，就送出表單（name為bodyform的form標籤）
     }
     else if ((height == '') && (weight != '')){
         alert('請輸入身高！');
@@ -18,15 +18,9 @@ function Empty() {
     } // 如果身高體重輸入的值 皆 為 空字串（身高、體重 皆未輸入）
 }
 
-// function prevent(evt) {
-//     if ((height == '') || (weight == '')){
-//         evt.preventDefault(); // 使用 event.preventDefault() 來中止原本的行為，然後做你想做的事
-//     }
-// }
-
 $('#calculateButton').click( evt => {
-    // evt.preventDefault(); // 使用 event.preventDefault() 來中止原本的行為，然後做你想做的事
-    Empty();
+    evt.preventDefault(); // 用 preventDefault 來取消「元素的預設行為」
+    Empty(); // 先透過 JS 去查詢表單有無錯誤（先做驗證，表單填寫有錯誤–> 就會彈跳提醒訊息），驗證完成後再透過 post 去傳送
 });
 // jQuery DOM元素操作
 // ES6箭頭函式
